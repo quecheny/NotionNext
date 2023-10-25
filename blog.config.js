16e1cf65d2310b1911269265a5b2ebdc0840fcae
andy yang <127816545+quecheny@users.noreply.github.com>
2023-10-25 19:52:39 +1100

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -17,7 +17,7 @@ const BLOG = {
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || true, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Andy Yang', // 您的昵称 例如 tangly1024
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Quechen Yang', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || 'A Normal Person🍚一个普通人', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://quecheny.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Sci, AI, RL, ML, Quant', // 网站关键词 英文逗号隔开
